d2c0409b49058974ae724065215564107db8ab1b
punit-pradhan <punit.pradhan@innoraft.com>
2023-03-06 11:45:27 +0530

    task 2 with oops

--- task2/index.php
+++ task2/index.php
@@ -7,20 +7,24 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
   <link rel="stylesheet" href="style.css">
-  <title>PHP-First-Task</title>
+  <title>PHP-Second-Task</title>
 </head>
 
 <body>
-  <form class="mainform" action="welcome.php" method="post">
-    <span>First Name:</span>
-    <input class="fname" type="text" name="firstname" pattern="[a-zA-Z]{1,}" required><br>
-    <span>Last Name:</span>
-    <input class="lname" type="text" name="lastname" pattern="[a-zA-Z]{1,}" required><br>
-    <span> Full Name:</span>
-    <input id="flname" type="text" disabled><br>
-    <input class="submitbtn" type="submit">
-  </form>
+  <div class="mainform">
+    <form action="welcome.php" method="post" enctype="multipart/form-data">
+      <span>First Name:</span>
+      <input class="fname" type="text" name="firstname" pattern="[a-zA-Z]{1,}" required><br>
+      <span>Last Name:</span>
+      <input class="lname" type="text" name="lastname" pattern="[a-zA-Z]{1,}" required><br>
+      <span> Full Name:</span>
+      <input id="flname" type="text" disabled><br>
+      <span> Image:</span>
+      <input class="fileupload" name="image" type="file" id="file" /><br>
 
+      <input class="submitbtn" type="submit">
+    </form>
+  </div>
   <script>
     $(document).ready(function() {
       $(".fname , .lname").on('input', function() {
